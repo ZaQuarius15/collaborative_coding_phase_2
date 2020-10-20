f74ae15a0a0c6ec60a51cdf22395fc33efd2f560
ZaQuarius15 <jzcarlson014@gmail.com>
2020-10-20 10:28:56 -0500

    Filtered events index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,17 @@
 class EventsController < ApplicationController
      
     def index
-        @events = Event.all
+        if params[:topic]
+            @events = Event.all.select do |e|
+                if e.language = params[:language] 
+                    e.topic = params[:topic]
+                end
+            end
+            @lang = Language.all.find(params[:language])
+            @topic = Topic.all.find(params[:topic])
+        else
+            @events = Event.all
+        end
     end
     
     def show
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,14 @@ class Event < ApplicationRecord
     belongs_to :host, :class_name => :User
     belongs_to :lang_topic
     has_many :participants, :class_name => :User
+
+    attr_reader :language, :topic
+
+    def language
+        self.lang_topic.language
+    end
+
+    def topic
+        self.lang_topic.topic
+    end
 end
