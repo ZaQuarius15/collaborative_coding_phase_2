91b8a20997be4ca6c6cf78abf455a9d1ddba723b
ZaQuarius15 <jzcarlson014@gmail.com>
2020-10-20 22:10:24 -0500

    Filtered events/index success

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,12 +3,18 @@ class EventsController < ApplicationController
     def index
         if params[:topic]
             @events = Event.all.select do |e|
-                if e.language = params[:language] 
-                    e.topic = params[:topic]
+                
+                if e.language == Language.find(params[:language]) 
+                    e.topic == Topic.find(params[:topic])
                 end
             end
             @lang = Language.all.find(params[:language])
             @topic = Topic.all.find(params[:topic])
+        elsif params[:language]
+            @events = Event.all.select do |e|
+                e.language == Language.find(params[:language])
+            end 
+            @lang = Language.all.find(params[:language])
         else
             @events = Event.all
         end
